6fb8e731076a9dd5ef750e77fe6fb0eb958e2f69
slickcodes <hartpaulisimo@gmail.com>
2022-11-05 09:28:32 +0100

    imported necessary components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Projects from './../components/Projects.svelte';
+	import Navigation from './../components/Navigation.svelte';
+	import Header from './../components/Header.svelte';
+</script>
+
+<svelte:head>
+	<!-- <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" /> -->
+	<link href="https://fonts.cdnfonts.com/css/raleway-5" rel="stylesheet" />
+</svelte:head>
+
+<main class="container">
+	<Navigation />
+	<Header />
+	<Projects />
+</main>
+
+<style>
+	/* your styles go here */
+	:global(*) {
+		font-family: 'raleway';
+		letter-spacing: 0.1em;
+	}
+</style>
