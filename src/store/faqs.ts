12ec3e2e18bcb5758b38534164fbb56be17ba83d
slickcodes <hartpaulisimo@gmail.com>
2022-11-05 13:52:54 +0100

    create the faqs.ts store

---
+++ src/store/faqs.ts
@@ -0,0 +1,30 @@
+import { writable, type Writable } from "svelte/store"
+
+
+
+export const faqState = writable(1)
+
+export const faqs = [
+    {
+        title: "What is Lauch to Go all about?",
+        body: "You are here because you're interested in being a part of our investors community Launch to Grow is making it possible to. <br> <br> micro- invest in the best early - stage tech startups in Africa.You are."
+    }, {
+        title: "What is Lauch to Go all about?",
+        body: "You are here because you're interested in being a part of our investors community Launch to Grow is making it possible to. <br> <br> micro- invest in the best early - stage tech startups in Africa.You are."
+    }, {
+        title: "What is Lauch to Go all about?",
+        body: "You are here because you're interested in being a part of our investors community Launch to Grow is making it possible to. <br> <br> micro- invest in the best early - stage tech startups in Africa.You are."
+    }, {
+        title: "What is Lauch to Go all about?",
+        body: "You are here because you're interested in being a part of our investors community Launch to Grow is making it possible to. <br> <br> micro- invest in the best early - stage tech startups in Africa.You are."
+    }, {
+        title: "What is Lauch to Go all about?",
+        body: "You are here because you're interested in being a part of our investors community Launch to Grow is making it possible to. <br> <br> micro- invest in the best early - stage tech startups in Africa.You are."
+    }, {
+        title: "What is Lauch to Go all about?",
+        body: "You are here because you're interested in being a part of our investors community Launch to Grow is making it possible to. <br> <br> micro- invest in the best early - stage tech startups in Africa.You are."
+    }, {
+        title: "What is Lauch to Go all about?",
+        body: "You are here because you're interested in being a part of our investors community Launch to Grow is making it possible to. <br> <br> micro- invest in the best early - stage tech startups in Africa.You are."
+    }
+]
