d1bd05a8a297fc84ce22f5e583516c07ad5d5d96
slickcodes <hartpaulisimo@gmail.com>
2022-11-05 13:52:40 +0100

    create the Footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import FAQs from './FAQs.svelte';
+	import HeaderBackground from './HeaderBackground.svelte';
+</script>
+
+<footer class="footer">
+	<HeaderBackground invert={true} />
+	<div class="footer__container">
+		<FAQs />
+	</div>
+</footer>
+
+<style lang="scss">
+	.footer {
+		position: relative;
+		padding: 3em 0;
+	}
+</style>
